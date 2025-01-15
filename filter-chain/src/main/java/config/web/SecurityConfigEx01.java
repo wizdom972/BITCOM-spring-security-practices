b47fcefc5ae506a58078d99a09d8104c02940984
wizdom972 <jihae0584@gmail.com>
2025-01-15 14:47:08 +0900

    📝 2025-01-15 수업 내용
    - Filter Chain

--- filter-chain/src/main/java/config/web/SecurityConfigEx01.java
+++ filter-chain/src/main/java/config/web/SecurityConfigEx01.java
@@ -1,31 +1,76 @@
 package config.web;
 
+import java.util.Arrays;
+import java.util.List;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.web.FilterChainProxy;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.util.AntPathMatcher;
+
 import filter.SecurityFilterEx01;
 import filter.SecurityFilterEx02;
 import filter.SecurityFilterEx03;
 import filter.SecurityFilterEx04;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
+import jakarta.servlet.Filter;
+import jakarta.servlet.http.HttpServletRequest;
 
 @Configuration
 public class SecurityConfigEx01 {
-    @Bean
-    public SecurityFilterEx01 securityFilterEx01() {
-        return new SecurityFilterEx01();
-    }
 
-    @Bean
-    public SecurityFilterEx02 securityFilterEx02() {
-        return new SecurityFilterEx02();
-    }
+	@Bean
+	public FilterChainProxy securityFilterChainProxy() {
+		List<SecurityFilterChain> securityFilterChains = Arrays.asList(new SecurityFilterChain() {
 
-    @Bean
-    public SecurityFilterEx03 securityFilterEx03() {
-        return new SecurityFilterEx03();
-    }
+			@Override
+			public boolean matches(HttpServletRequest request) {
+				String uri = request.getRequestURI().replaceAll(request.getContextPath(), "");
 
-    @Bean
-    public SecurityFilterEx04 securityFilterEx04() {
-        return new SecurityFilterEx04();
-    }
+				return new AntPathMatcher().match("/hello/**", uri);
+			}
+
+			@Override
+			public List<Filter> getFilters() {
+				return Arrays.asList(securityFilterEx01(), securityFilterEx02());
+			}
+
+		}, new SecurityFilterChain() {
+
+			@Override
+			public boolean matches(HttpServletRequest request) {
+				String uri = request.getRequestURI().replaceAll(request.getContextPath(), "");
+
+				return new AntPathMatcher().match("/ping/**", uri);
+			}
+
+			@Override
+			public List<Filter> getFilters() {
+				return Arrays.asList(securityFilterEx01(), securityFilterEx02());
+			}
+
+		});
+
+		return new FilterChainProxy(securityFilterChains);
+	}
+
+	@Bean
+	public SecurityFilterEx01 securityFilterEx01() {
+		return new SecurityFilterEx01();
+	}
+
+	@Bean
+	public SecurityFilterEx02 securityFilterEx02() {
+		return new SecurityFilterEx02();
+	}
+
+	@Bean
+	public SecurityFilterEx03 securityFilterEx03() {
+		return new SecurityFilterEx03();
+	}
+
+	@Bean
+	public SecurityFilterEx04 securityFilterEx04() {
+		return new SecurityFilterEx04();
+	}
 }
--- filter-chain/src/main/java/config/web/SecurityConfigEx02.java
+++ filter-chain/src/main/java/config/web/SecurityConfigEx02.java
@@ -1,31 +1,51 @@
 package config.web;
 
+import java.util.Arrays;
+import java.util.List;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.web.DefaultSecurityFilterChain;
+import org.springframework.security.web.FilterChainProxy;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+
 import filter.SecurityFilterEx01;
 import filter.SecurityFilterEx02;
 import filter.SecurityFilterEx03;
 import filter.SecurityFilterEx04;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class SecurityConfigEx02 {
-    @Bean
-    public SecurityFilterEx01 securityFilterEx01() {
-        return new SecurityFilterEx01();
-    }
+	
+	@Bean
+	public FilterChainProxy securityFilterChainProxy() {
+		List<SecurityFilterChain> securityFilterChains = Arrays.asList(
+				new DefaultSecurityFilterChain(new AntPathRequestMatcher("/hello/**"), securityFilterEx01(),
+						securityFilterEx02()),
+				new DefaultSecurityFilterChain(new AntPathRequestMatcher("/ping/**"), securityFilterEx03(),
+						securityFilterEx04()));
 
-    @Bean
-    public SecurityFilterEx02 securityFilterEx02() {
-        return new SecurityFilterEx02();
-    }
+		return new FilterChainProxy(securityFilterChains);
+	}
 
-    @Bean
-    public SecurityFilterEx03 securityFilterEx03() {
-        return new SecurityFilterEx03();
-    }
+	@Bean
+	public SecurityFilterEx01 securityFilterEx01() {
+		return new SecurityFilterEx01();
+	}
 
-    @Bean
-    public SecurityFilterEx04 securityFilterEx04() {
-        return new SecurityFilterEx04();
-    }
+	@Bean
+	public SecurityFilterEx02 securityFilterEx02() {
+		return new SecurityFilterEx02();
+	}
+
+	@Bean
+	public SecurityFilterEx03 securityFilterEx03() {
+		return new SecurityFilterEx03();
+	}
+
+	@Bean
+	public SecurityFilterEx04 securityFilterEx04() {
+		return new SecurityFilterEx04();
+	}
 }
